dca48255d45b4cdacade42b1ae7c815cba75890c
AlexH <ahoran2020@fau.edu>
2025-11-28 15:57:29 -0500

    Fixed?

--- Epoch.py
+++ Epoch.py
@@ -6,7 +6,7 @@ def train(n = 1, fileName = "BreakoutAI.py"):
     path = os.path.dirname(os.path.realpath(__file__))
 
     for i in range(n):
-       subprocess.run(f"py ./{fileName}", cwd=path)
+       subprocess.run(f"py ./{fileName} &", cwd=path)
        print(f"EPOCH {i}/{n}")
 
 
